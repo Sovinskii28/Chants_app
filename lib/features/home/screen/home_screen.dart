9e8bc17dc82e9fa4f1a2109449b4d5cad8f9f5ec
Sovinskii28 <kurmllil21@gmail.com>
2025-05-07 03:18:33 +0300

    появление надписи 'они не ведают'

--- lib/features/home/screen/home_screen.dart
+++ lib/features/home/screen/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -12,6 +14,33 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool _showTextOnImage = false;
+  Timer? _textTimer;
+
+  @override
+  void dispose() {
+    _textTimer?.cancel(); // Отменяем таймер при уничтожении виджета
+    super.dispose();
+  }
+
+  void _handleImageTap() {
+    setState(() {
+      _showTextOnImage = true;
+
+      // Отменяем предыдущий таймер, если он был
+      _textTimer?.cancel();
+
+      // Устанавливаем таймер на 3 секунды для скрытия текста
+      _textTimer = Timer(const Duration(seconds: 3), () {
+        if (mounted) {
+          setState(() {
+            _showTextOnImage = false;
+          });
+        }
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
@@ -20,71 +49,86 @@ class _MyHomePageState extends State<MyHomePage> {
       body: SafeArea(
         child: Column(
           children: [
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height / 2.5,
-              child: SvgPicture.asset(
-                'lib/assets/img/png/Jesus.svg',
-                fit: BoxFit.cover,
+            GestureDetector(
+              onTap: _handleImageTap,
+              child: Stack(
+                alignment: Alignment.topLeft,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height / 2.5,
+                    child: SvgPicture.asset(
+                      'lib/assets/img/png/Jesus.svg',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  AnimatedOpacity(
+                    opacity: _showTextOnImage ? 1.0 : 0.0,
+                    duration: const Duration(milliseconds: 700),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 15,top: 15),
+                      child: Text(
+                        'Отец, прости им, ибо они не ведают, что творят',
+                        style: theme.textTheme.titleLarge?.copyWith(
+                          color: Colors.yellow,
+                          shadows: [
+                            Shadow(
+                              blurRadius: 10.0,
+                              color: Colors.black,
+                              offset: Offset(2.0, 2.0),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-
             const SizedBox(height: 26),
-            Text(
-              'Господи Иисусе Христе',
-              style: theme.textTheme.titleLarge,
-            ),
+            Text('Господи Иисусе Христе', style: theme.textTheme.titleLarge),
             const SizedBox(height: 26),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-              SizedBox(
-                width: 160,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(
-                        12,
-                      ), // Скругление углов
+                SizedBox(
+                  width: 160,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 24,
+                        vertical: 12,
+                      ),
+                      backgroundColor: Colors.amber,
+                      foregroundColor: Colors.black,
                     ),
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 24,
-                      vertical: 12,
-                    ), // Отступы
-                    backgroundColor: Colors.amber, // Цвет фона (опционально)
-                    foregroundColor: Colors.black, // Цвет текста (опционально)
+                    child: Text('Молитвы', style: theme.textTheme.bodyLarge),
                   ),
-                  child: Text(
-                    'Молитвы',
-                    style: theme.textTheme.bodyLarge,
-                    ),
-                ),
                 ),
                 SizedBox(
                   width: 190,
                   child: ElevatedButton(
-                  onPressed: () {
-                    context.router.push(const PrayersRoute());
-                  },
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(
-                        12,
-                      ), // Скругление углов
+                    onPressed: () {
+                      context.router.push(const PrayersRoute());
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 24,
+                        vertical: 12,
+                      ),
+                      backgroundColor: Colors.amber,
+                      foregroundColor: Colors.black,
                     ),
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 24,
-                      vertical: 12,
-                    ), // Отступы
-                    backgroundColor: Colors.amber, // Цвет фона (опционально)
-                    foregroundColor: Colors.black, // Цвет текста (опционально)
+                    child: Text('Песнопения', style: theme.textTheme.bodyLarge),
                   ),
-                  child: Text(
-                    'Песнопения',
-                    style: theme.textTheme.bodyLarge,
-                    ),
-                ),)
+                ),
               ],
             ),
           ],
